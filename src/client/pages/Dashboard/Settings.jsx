e17f39364e54aace9d4c72440a6d2b004978bb9b
Max McClaskie <max.mcclaskie@gmail.com>
2022-06-09 10:31:20 -0400

    added bot settings field values and form submission

--- src/client/pages/Dashboard/Settings.jsx
+++ src/client/pages/Dashboard/Settings.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Button, Radio, RadioGroup, Select, NumberInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
 
 import Label from '../../components/Label.jsx';
 import Text from '../../components/Text.jsx';
@@ -12,76 +13,148 @@ const StyledRow = styled(Row)`
 `;
 
 const Controls = () => {
+    const form = useForm({
+        initialValues: {
+            listPrice: '',
+            monstersRegularUnit: '',
+            monstersRegularOperator: '',
+            monstersRegularAmount: '',
+            monstersGoldUnit: '',
+            monstersGoldOperator: '',
+            monstersGoldAmount: '',
+            summonersRegularUnit: '',
+            summonersRegularOperator: '',
+            summonersRegularAmount: '',
+            summonersGoldUnit: '',
+            summonersGoldOperator: '',
+            summonersGoldAmount: '',
+        },
+    });
+
+    const handleSubmit = (values) => {
+        console.log(values);
+    };
     return (
         <DashboardPage>
             <h1>Settings</h1>
-            <Label>
-                <Text>List Price</Text>
-                <RadioGroup>
-                    <Radio value="low" label="Undercut lowest price" />
-                    <Radio value="average" label="Undercut average price" />
-                </RadioGroup>
-            </Label>
+            <form onSubmit={form.onSubmit(handleSubmit)}>
+                <Label>
+                    <Text>List Price</Text>
+                    <RadioGroup
+                        {...form.getInputProps('listPrice', { type: 'radio' })}
+                    >
+                        <Radio value="low" label="Undercut lowest price" />
+                        <Radio value="average" label="Undercut average price" />
+                    </RadioGroup>
+                </Label>
 
-            <Label>
-                <Text>Monsters - Regular</Text>
-                <Row>
-                    <Select
-                        style={{ paddingRight: '16px' }}
-                        data={['CP', 'BCX']}
-                    />
-                    <Select
-                        style={{ paddingRight: '16px' }}
-                        data={['Greater than', 'Equal to', 'Less than']}
-                    />
-                    <NumberInput style={{ width: '72px' }} />
-                </Row>
-            </Label>
+                <Label>
+                    <Text>Monsters - Regular</Text>
+                    <Row>
+                        <Select
+                            style={{ paddingRight: '16px' }}
+                            data={['CP', 'BCX']}
+                            {...form.getInputProps('monstersRegularUnit', {
+                                type: 'select',
+                            })}
+                        />
+                        <Select
+                            style={{ paddingRight: '16px' }}
+                            data={['Greater than', 'Equal to', 'Less than']}
+                            {...form.getInputProps('monstersRegularOperator', {
+                                type: 'select',
+                            })}
+                        />
+                        <NumberInput
+                            style={{ width: '72px' }}
+                            {...form.getInputProps('monstersRegularAmount', {
+                                type: 'number',
+                            })}
+                        />
+                    </Row>
+                </Label>
 
-            <Label>
-                <Text>Monsters - Gold</Text>
-                <Row>
-                    <Select
-                        style={{ paddingRight: '16px' }}
-                        data={['CP', 'BCX']}
-                    />
-                    <Select
-                        style={{ paddingRight: '16px' }}
-                        data={['Greater than', 'Equal to', 'Less than']}
-                    />
-                    <NumberInput style={{ width: '72px' }} />
-                </Row>
-            </Label>
+                <Label>
+                    <Text>Monsters - Gold</Text>
+                    <Row>
+                        <Select
+                            style={{ paddingRight: '16px' }}
+                            data={['CP', 'BCX']}
+                            {...form.getInputProps('monstersGoldUnit', {
+                                type: 'select',
+                            })}
+                        />
+                        <Select
+                            style={{ paddingRight: '16px' }}
+                            data={['Greater than', 'Equal to', 'Less than']}
+                            {...form.getInputProps('monstersGoldOperator', {
+                                type: 'select',
+                            })}
+                        />
+                        <NumberInput
+                            style={{ width: '72px' }}
+                            {...form.getInputProps('monstersGoldAmount', {
+                                type: 'number',
+                            })}
+                        />
+                    </Row>
+                </Label>
 
-            <Label>
-                <Text>Summoners - Regular</Text>
-                <Row>
-                    <Select
-                        style={{ paddingRight: '16px' }}
-                        data={['CP', 'BCX']}
-                    />
-                    <Select
-                        style={{ paddingRight: '16px' }}
-                        data={['Greater than', 'Equal to', 'Less than']}
-                    />
-                    <NumberInput style={{ width: '72px' }} />
-                </Row>
-            </Label>
+                <Label>
+                    <Text>Summoners - Regular</Text>
+                    <Row>
+                        <Select
+                            style={{ paddingRight: '16px' }}
+                            data={['CP', 'BCX']}
+                            {...form.getInputProps('summonersRegularUnit', {
+                                type: 'select',
+                            })}
+                        />
+                        <Select
+                            style={{ paddingRight: '16px' }}
+                            data={['Greater than', 'Equal to', 'Less than']}
+                            {...form.getInputProps('summonersRegularOperator', {
+                                type: 'select',
+                            })}
+                        />
+                        <NumberInput
+                            style={{ width: '72px' }}
+                            {...form.getInputProps('summonersRegularAmount', {
+                                type: 'number',
+                            })}
+                        />
+                    </Row>
+                </Label>
 
-            <Label>
-                <Text>Summoners - Gold</Text>
-                <Row>
-                    <Select
-                        style={{ paddingRight: '16px' }}
-                        data={['CP', 'BCX']}
-                    />
-                    <Select
-                        style={{ paddingRight: '16px' }}
-                        data={['Greater than', 'Equal to', 'Less than']}
-                    />
-                    <NumberInput style={{ width: '72px' }} />
-                </Row>
-            </Label>
+                <Label>
+                    <Text>Summoners - Gold</Text>
+                    <Row>
+                        <Select
+                            style={{ paddingRight: '16px' }}
+                            data={['CP', 'BCX']}
+                            {...form.getInputProps('summonersGoldUnit', {
+                                type: 'select',
+                            })}
+                        />
+                        <Select
+                            style={{ paddingRight: '16px' }}
+                            data={['Greater than', 'Equal to', 'Less than']}
+                            {...form.getInputProps('summonersGoldOperator', {
+                                type: 'select',
+                            })}
+                        />
+                        <NumberInput
+                            style={{ width: '72px' }}
+                            {...form.getInputProps('summonersRegularAmount', {
+                                type: 'number',
+                            })}
+                        />
+                    </Row>
+                </Label>
+                <Button style={{ marginTop: '2em' }} type="submit">
+                    Save Settings
+                </Button>
+            </form>
             <Label>
                 <Text>Bot Status</Text>
                 <StyledRow>
