beedddee9b09903ae4b2517aef26b3abbabdd404
Logan Bernard <bernard.logan4@gmail.com>
2022-06-10 11:20:15 -0400

    hive hello world

--- src/api/services/hive.js
+++ src/api/services/hive.js
@@ -1,3 +1,47 @@
-// import axios from 'axios';
+import axios from 'axios';
+import { Client, PrivateKey } from '@hiveio/dhive';
 
-// const
+const hiveTest = async () => {
+    const client = new Client([
+        'https://api.hive.blog',
+        'https://api.hivekings.com',
+        'https://anyx.io',
+        'https://api.openhive.network',
+    ]);
+
+    const key = PrivateKey.from();
+    // const key = PrivateKey.fromLogin("username", "password", "posting");
+
+    // hive.broadcast.claimRewardBalance(
+    //     wif,
+    //     account,
+    //     reward_hive,
+    //     reward_hbd,
+    //     reward_vests,
+    //     callback
+    // );
+
+    const res = await client.broadcast.json(
+        {
+            required_posting_auths: [],
+            required_auths: ['chigginn'],
+            id: 'sm_market_list',
+            json: JSON.stringify({
+                cards: [['C3-331-20B93BXDG0', 200]],
+                type: 'rent',
+                fee: 500,
+                required_posting_auths: [],
+                required_auths: ['chigginn'],
+            }),
+        },
+        key
+    );
+    console.log('RESULT');
+    console.log('-------------------------------');
+    console.log(res);
+    console.log('-------------------------------');
+};
+
+export default {
+    hiveTest,
+};
