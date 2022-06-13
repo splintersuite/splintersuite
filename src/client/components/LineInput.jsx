3f7c124e4e37266f4ec596ed0ec295337bf35a86
Logan Bernard <bernard.logan4@gmail.com>
2022-06-13 17:11:01 -0400

    settings page

---
+++ src/client/components/LineInput.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+import Label from '../components/Label.jsx';
+
+const Container = styled.div`
+    display: flex;
+    width: 300px;
+    padding: ${({ theme }) => theme.space(1)};
+    margin-right: ${({ theme }) => theme.space(4)};
+    background: ${({ theme }) => theme.colors.grey[900]};
+    border-radius: 8px;
+`;
+
+const InputLabel = styled(Label)`
+    margin-right: auto;
+    opacity: 1;
+`;
+
+const Input = styled.input`
+    width: 51px;
+    padding-left: ${({ theme }) => theme.space(2)};
+    margin-left: ${({ theme }) => theme.space(1)};
+    color: white;
+    font-size: 16px;
+    font-weight: bold;
+    font-family: 'Source Sans Pro';
+    outline: none;
+    border: none;
+    border-left: ${({ theme }) => `2px solid ${theme.colors.grey[800]}`};
+    background: transparent;
+`;
+
+const LineInput = ({ label, min, max, ...props }) => {
+    return (
+        <Container>
+            <InputLabel>{label}</InputLabel>
+            <Input {...props} min={min} max={max} type="number" />
+        </Container>
+    );
+};
+
+export default LineInput;
--- src/client/pages/Dashboard/Settings.jsx
+++ src/client/pages/Dashboard/Settings.jsx
@@ -1,204 +1,157 @@
 import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import { Button, NumberInput, Indicator } from '@mantine/core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRobot } from '@fortawesome/free-solid-svg-icons';
 import { useForm } from '@mantine/form';
 
 import Label from '../../components/Label.jsx';
 import Text from '../../components/Text.jsx';
 import Row from '../../components/Row.jsx';
+import Card from '../../components/Card.jsx';
+import LineInput from '../../components/LineInput.jsx';
 import DashboardPage from '../../components/DashboardPage.jsx';
 import { useBot } from '../../contexts/BotContext.jsx';
 
-const StyledRow = styled(Row)`
-    width: 200px;
-    justify-content: flex-start;
+const SectionSubheader = styled(Text)`
+    margin-bottom: ${({ theme }) => theme.space(4)};
 `;
 
-const StyledNumber = styled(NumberInput)`
-    width: 72px;
-`;
-
-const StartButton = styled(Button)`
-    min-width: 100px;
-    margin-right: ${({ theme }) => theme.space(4)};
-`;
-
-const SectionHeader = styled.h2`
-    margin-top: ${({ theme }) => theme.space(6)};
-`;
-
-const StyledLabel = styled(Label)`
-    font-size: 16px;
-    width: 256px;
-`;
-
-const FirstLabel = styled(StyledLabel)`
-    margin-top: ${({ theme }) => theme.space(2)};
+const InputRow = styled(Row)`
+    margin-bottom: ${({ theme }) => theme.space(4)};
 `;
 
 const Settings = () => {
-    const [botStatusColor, setBotStatusColor] = useState('red');
-    const [botStatusText, setBotStatusText] = useState('Start');
-    const { botActive, botSettings, toggleBotActive, updateBotSettings } =
-        useBot();
+    const { botSettings, updateBotSettings } = useBot();
     const form = useForm({
         initialValues: {
             ...botSettings,
         },
     });
 
-    useEffect(() => {
-        if (botActive) {
-            setBotStatusColor('green');
-            setBotStatusText('Stop');
-        } else {
-            setBotStatusColor('red');
-            setBotStatusText('Start');
-        }
-    }, [botActive]);
-
     const handleSubmit = (values) => {
-        updateBotSettings(values);
+        console.log('submit');
+        const formattedValues = {};
+        for (const [key, value] of Object.entries(values)) {
+            formattedValues[key] = parseInt(value);
+        }
+        updateBotSettings(formattedValues);
     };
 
     return (
         <DashboardPage>
-            <h1>Settings</h1>
-            <form onSubmit={form.onSubmit(handleSubmit)}>
-                <SectionHeader>Choose Minimum Level</SectionHeader>
+            <Card>
                 <Row>
-                    <FirstLabel>
-                        <Text>Common Monsters</Text>
-                        <StyledNumber
+                    <h1>Settings</h1>
+                    <Button
+                        style={{ marginLeft: 'auto' }}
+                        onClick={form.onSubmit(handleSubmit)}
+                    >
+                        Save Settings
+                    </Button>
+                </Row>
+            </Card>
+
+            <form onSubmit={form.onSubmit(handleSubmit)}>
+                <Card>
+                    <h2>Choose Minimum Levels</h2>
+                    <SectionSubheader>
+                        All monsters greater than or equal to the selected level
+                        will be listed for rental.
+                    </SectionSubheader>
+
+                    <InputRow>
+                        <LineInput
+                            label="Common Monsters"
                             min={0}
                             max={10}
                             {...form.getInputProps('commonNorm', {
                                 type: 'number',
                             })}
                         />
-                    </FirstLabel>
-                    <FirstLabel>
-                        <Text>Common Monsters - Gold</Text>
-                        <StyledNumber
+                        <LineInput
+                            label="Common Monsters - Gold"
                             min={0}
                             max={10}
                             {...form.getInputProps('commonGold', {
                                 type: 'number',
                             })}
                         />
-                    </FirstLabel>
-                </Row>
+                    </InputRow>
 
-                <Row>
-                    <StyledLabel>
-                        <Text>Rare Monsters</Text>
-                        <StyledNumber
+                    <InputRow>
+                        <LineInput
+                            label="Rare Monsters"
                             min={0}
                             max={8}
                             {...form.getInputProps('rareNorm', {
                                 type: 'number',
                             })}
                         />
-                    </StyledLabel>
-
-                    <StyledLabel>
-                        <Text>Rare Monsters - Gold</Text>
-                        <StyledNumber
+                        <LineInput
+                            label="Rare Monsters - Gold"
                             min={0}
                             max={8}
                             {...form.getInputProps('rareGold', {
                                 type: 'number',
                             })}
                         />
-                    </StyledLabel>
-                </Row>
+                    </InputRow>
 
-                <Row>
-                    <StyledLabel>
-                        <Text>Epic Monsters</Text>
-                        <StyledNumber
+                    <InputRow>
+                        <LineInput
+                            label="Epic Monsters"
                             min={0}
                             max={6}
                             {...form.getInputProps('epicNorm', {
                                 type: 'number',
                             })}
                         />
-                    </StyledLabel>
-
-                    <StyledLabel>
-                        <Text>Epic Monsters - Gold</Text>
-                        <StyledNumber
+                        <LineInput
+                            label="Epic Monsters - Gold"
                             min={0}
                             max={6}
                             {...form.getInputProps('epicGold', {
                                 type: 'number',
                             })}
                         />
-                    </StyledLabel>
-                </Row>
+                    </InputRow>
 
-                <Row>
-                    <StyledLabel>
-                        <Text>Legendary Monsters</Text>
-                        <StyledNumber
+                    <Row>
+                        <LineInput
+                            label="Legendary Monsters"
                             min={0}
                             max={4}
                             {...form.getInputProps('legendaryNorm', {
                                 type: 'number',
                             })}
                         />
-                    </StyledLabel>
-
-                    <StyledLabel>
-                        <Text>Legendary Monsters - Gold</Text>
-                        <StyledNumber
+                        <LineInput
+                            label="Legendary Monsters - Gold"
                             min={0}
                             max={4}
                             {...form.getInputProps('legendaryGold', {
                                 type: 'number',
                             })}
                         />
-                    </StyledLabel>
-                </Row>
-
-                <Label>
-                    <Text>Daily Relistings</Text>
-                    <StyledNumber
-                        min={0}
-                        {...form.getInputProps('dailyRelistings', {
-                            type: 'number',
-                        })}
-                    />
-                </Label>
-
-                <Button style={{ marginTop: '2em' }} type="submit">
-                    Save Settings
-                </Button>
+                    </Row>
+                </Card>
             </form>
 
-            <Label onClick={(e) => e.preventDefault()}>
-                <StyledRow>
-                    <Text>Bot Status</Text>
-                </StyledRow>
-                <StyledRow>
-                    <StartButton
-                        color={botActive ? 'red' : 'green'}
-                        size="lg"
-                        onClick={toggleBotActive}
-                    >
-                        {botStatusText}
-                    </StartButton>
-                    <Indicator
-                        color={botStatusColor}
-                        size={14}
-                        style={{ margin: '1em 0' }}
-                    >
-                        <FontAwesomeIcon size={'2x'} icon={faRobot} />
-                    </Indicator>
-                </StyledRow>
-            </Label>
+            <Card>
+                <h2>Choose Daily Relistings</h2>
+                <SectionSubheader>
+                    This will determine how frequently the bot attempts to
+                    relist your cards.
+                </SectionSubheader>
+
+                <LineInput
+                    label="Daily Relistings"
+                    min={1}
+                    max={24}
+                    {...form.getInputProps('dailyRelistings', {
+                        type: 'number',
+                    })}
+                />
+            </Card>
         </DashboardPage>
     );
 };
