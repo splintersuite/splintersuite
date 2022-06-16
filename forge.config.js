e78be6aec0dbc4aba86df5c764f65aa7907873c2
Logan Bernard <bernard.logan4@gmail.com>
2022-06-16 19:57:58 -0400

    action test

---
+++ forge.config.js
@@ -0,0 +1,64 @@
+const config = {
+    packagerConfig: {
+        icon: './src/client/assets/icons/icon',
+        name: 'SplinterSuite',
+        executableName: 'splintersuite',
+    },
+    makers: [
+        {
+            name: '@electron-forge/maker-squirrel',
+            config: {
+                description: 'Rent your Splinterlands cards with ease.',
+            },
+        },
+        {
+            name: '@electron-forge/maker-zip',
+            platforms: ['darwin'],
+        },
+        {
+            name: '@electron-forge/maker-deb',
+            config: {
+                icon: {
+                    scalable: './src/client/assets/icons/icon.svg',
+                },
+                description: 'Rent your Splinterlands cards with ease.',
+            },
+        },
+        {
+            name: '@electron-forge/maker-rpm',
+            config: {
+                icon: {
+                    scalable: './src/client/assets/icons/icon.svg',
+                },
+                description: 'Rent your Splinterlands cards with ease.',
+            },
+        },
+    ],
+    plugins: [
+        [
+            '@electron-forge/plugin-webpack',
+            {
+                mainConfig: './webpack.main.config.js',
+                devContentSecurityPolicy:
+                    "connect-src 'self' https://api2.splinterlands.com 'unsafe-eval'",
+                renderer: {
+                    config: './webpack.renderer.config.js',
+                    entryPoints: [
+                        {
+                            html: './src/client/index.html',
+                            js: './src/client/renderer.js',
+                            name: 'main_window',
+                        },
+                        {
+                            html: './src/bot/index.html',
+                            js: './src/bot/renderer.js',
+                            name: 'bot_window',
+                        },
+                    ],
+                },
+            },
+        ],
+    ],
+};
+
+module.exports = config;
--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from '@emotion/react';
 import { MantineProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
 
 import { UserProvider } from '../contexts/UserContext.jsx';
 import { BotProvider } from '../contexts/BotContext.jsx';
@@ -12,13 +13,15 @@ const App = () => {
         <MantineProvider
             theme={{ primaryColor: 'violet', fontFamily: 'Source Sans Pro' }}
         >
-            <ThemeProvider theme={theme}>
-                <UserProvider>
-                    <BotProvider>
-                        <Routes />
-                    </BotProvider>
-                </UserProvider>
-            </ThemeProvider>
+            <NotificationsProvider position="top-right">
+                <ThemeProvider theme={theme}>
+                    <UserProvider>
+                        <BotProvider>
+                            <Routes />
+                        </BotProvider>
+                    </UserProvider>
+                </ThemeProvider>
+            </NotificationsProvider>
         </MantineProvider>
     );
 };
