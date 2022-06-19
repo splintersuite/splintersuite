7baa016994ab701925e76a39c49a98614a8acaac
Logan Bernard <bernard.logan4@gmail.com>
2022-06-18 20:48:31 -0400

    bump

--- src/client/pages/Landing.jsx
+++ src/client/pages/Landing.jsx
@@ -44,7 +44,7 @@ const Landing = () => {
     const { username, handleLogin } = useUser();
     const navigate = useNavigate();
 
-    console.log('update landed');
+    console.log('update 8');
 
     const form = useForm({
         initialValues: {
