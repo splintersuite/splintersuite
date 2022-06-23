b10fb4bb8cf3136f3196b37d3292190a861fd424
Logan Bernard <bernard.logan4@gmail.com>
2022-06-23 11:52:40 -0400

    loading page

--- src/client/components/Routes.jsx
+++ src/client/components/Routes.jsx
@@ -6,28 +6,20 @@ import {
     Navigate,
 } from 'react-router-dom';
 
+import Loading from '../pages/Loading.jsx';
 import Landing from '../pages/Landing.jsx';
 import Dashboard from '../pages/Dashboard/Dashboard.jsx';
 import DashboardSettings from '../pages/Dashboard/Settings.jsx';
 import DashboardStats from '../pages/Dashboard/Stats.jsx';
 import DashboardAccount from '../pages/Dashboard/Account.jsx';
 import DashboardBilling from '../pages/Dashboard/Billing.jsx';
-import { useUser } from '../contexts/UserContext.jsx';
 
 const RoutesContainer = () => {
-    const { username } = useUser();
-
-    const defaultRoute = !username ? (
-        <Navigate to="/home" />
-    ) : (
-        <Navigate to="/app" />
-    );
-
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<Loading />} />
                 <Route path="/home" element={<Landing />} />
-                <Route path="/" element={defaultRoute} />
                 <Route path="/app" element={<Dashboard />}>
                     <Route
                         path="/app"
