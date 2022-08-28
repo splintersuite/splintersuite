9afce5e9ad7849834f3c8df3dee24340552613ad
hackinhukk <trevorthomas@mac.com>
2022-08-28 18:19:51 -0400

    getting rid of commented out code

--- src/bot/server/services/activeRentals.js
+++ src/bot/server/services/activeRentals.js
@@ -5,18 +5,15 @@ const splinterlandsService = require('./splinterlands');
 const activeRentalCardsInfoByRentalTx = ({ activeRentals }) => {
     try {
         //  console.log('activeRentalCardsInfoByRentalTx start');
-        // rentalTx is not unique, sellTx is unique, therefor getting rid of the rental_tx
-        //  const activeRentalsByRentalTx = {};
+        // rentalTx is not unique, sellTx is unique
         const activeRentalsBySellTrxId = {};
 
         activeRentals.forEach((rental) => {
             const { sell_trx_id } = rental;
-            //const { rental_tx, sell_trx_id } = rental;
-            // activeRentalsByRentalTx[rental_tx] = rental;
             activeRentalsBySellTrxId[sell_trx_id] = rental;
         });
+
         return { activeRentalsBySellTrxId };
-        // return { activeRentalsByRentalTx, activeRentalsBySellTrxId };
     } catch (err) {
         window.api.bot.log({
             message: `/bot/server/services/activeRentals/activeRentalCardsInfoByRentalTx error: ${err.message}`,
@@ -28,11 +25,7 @@ const activeRentalCardsInfoByRentalTx = ({ activeRentals }) => {
 const getActiveRentalsByRentalId = async (username) => {
     try {
         //sByRentalId start');
-        /*
-      02:38:52.668 › [27/08/2022 02:38:52 -04:00] Active Rentals: 19135
-02:38:52.668 › [27/08/2022 02:38:52 -04:00] By Rental Tx: 11253
-02:38:52.670 › [27/08/2022 02:38:52 -04:00] By Sell Tx: 19135
-*/
+
         const activeRentals = await splinterlandsService.activeRentalCardsInfo(
             username
         );
@@ -40,6 +33,7 @@ const getActiveRentalsByRentalId = async (username) => {
         const { activeRentalsBySellTrxId } = activeRentalCardsInfoByRentalTx({
             activeRentals,
         });
+
         window.api.bot.log({
             message: `/bot/server/services/activeRentals/getActiveRentalsByRentalId`,
         });
