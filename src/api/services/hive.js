821ac4ffb42ead3d0f537749f0fc766ec1c70252
hackinhukk <trevorthomas@mac.com>
2022-11-03 21:30:26 -0400

    posting auth check in hive

--- src/api/services/hive.js
+++ src/api/services/hive.js
@@ -8,12 +8,32 @@ const client = new Client([
     'https://anyx.io',
     'https://api.openhive.network',
 ]);
+const SS_key = process.env.SUITE_POST_KEY;
+const SS_account_name = 'splintersuite';
+
+const isPostingAuthDelegated = async ({ username }) => {
+    const data = await client.database.getAccounts([username]);
+    let delegated = false;
+    const postingAccountAuths = data[0]?.posting?.account_auths;
+    for (const account of postingAccountAuths) {
+        if (account[0] === SS_account_name) {
+            delegated = true;
+            break;
+        }
+    }
+    return delegated;
+};
 
 const createRentals = async (cards) => {
     const username = userService.getUsername();
-    const rawKey = await userService.getKey(username);
+    const delegated = isPostingAuthDelegated({ username });
+    let rawKey;
+    if (delegated) {
+        rawKey = SS_key;
+    } else {
+        rawKey = await userService.getKey(username);
+    }
     const key = PrivateKey.from(rawKey);
-
     const res = await client.broadcast.json(
         {
             required_posting_auths: [username],
@@ -33,7 +53,14 @@ const createRentals = async (cards) => {
 
 const updateRentals = async (cards) => {
     const username = userService.getUsername();
-    const rawKey = await userService.getKey(username);
+    const delegated = isPostingAuthDelegated({ username });
+    let rawKey;
+    if (delegated) {
+        rawKey = SS_key;
+    } else {
+        rawKey = await userService.getKey(username);
+    }
+    //const rawKey = await userService.getKey(username);
     const key = PrivateKey.from(rawKey);
 
     const res = await client.broadcast.json(
@@ -54,7 +81,14 @@ const updateRentals = async (cards) => {
 
 const relistActiveRentals = async (cards) => {
     const username = userService.getUsername();
-    const rawKey = await userService.getKey(username);
+    const delegated = isPostingAuthDelegated({ username });
+    let rawKey;
+    if (delegated) {
+        rawKey = SS_key;
+    } else {
+        rawKey = await userService.getKey(username);
+    }
+    //const rawKey = await userService.getKey(username);
     const key = PrivateKey.from(rawKey);
 
     const res = await client.broadcast.json(
@@ -75,7 +109,14 @@ const relistActiveRentals = async (cards) => {
 
 const deleteRentals = async (cards) => {
     const username = userService.getUsername();
-    const rawKey = await userService.getKey(username);
+    const delegated = isPostingAuthDelegated({ username });
+    let rawKey;
+    if (delegated) {
+        rawKey = SS_key;
+    } else {
+        rawKey = await userService.getKey(username);
+    }
+    //  const rawKey = await userService.getKey(username);
     const key = PrivateKey.from(rawKey);
 
     const res = await client.broadcast.json(
