baf95de457ea4be75337ffa5378499b40c8a26a4
jboxxx <jdealtrey@gmail.com>
2022-11-06 20:52:41 -0500

    better comments

--- src/client/util/constants.js
+++ src/client/util/constants.js
@@ -4,7 +4,7 @@ export const errors = {
 
 export const alerts = {
     delegation:
-        "From our test of the hive blockchain, it does not appear that the entered username has delegated 'splintersuite' posting authority.  Double check your username spelling and/or refer to our blogpost for information on delegating posting authority, or alternatively use splintersuite with your posting key.",
+        "The entered username has not delegated 'splintersuite' posting authority.  Double check your username spelling and/or refer to our blogpost for information on delegating posting authority.",
     postingKey:
-        'The Posting Key you entered does not appear to be a valid key for the hive blockchain.   Are you sure you entered it properly?',
+        'The Posting Key you entered is not a valid key for the hive blockchain.   Are you sure you entered it properly?',
 };
